98d157e42b327bb400242b334f96c2bd4fd48e94
John Romani <john@anthood.com>
2019-06-30 05:34:40 +0200

    postcss too

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
 module.exports = {
   plugins: [
     new HtmlWebPackPlugin({
       template: "./src/index.html",
-      filename: "./index.html",
+      filename: "./index.html"
     }),
     new MiniCssExtractPlugin({
       filename: "[name].css",
-      chunkFilename: "[id].css",
-    }),
+      chunkFilename: "[id].css"
+    })
   ],
   module: {
     rules: [
@@ -18,27 +18,28 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
-        },
+          loader: "babel-loader"
+        }
       },
       {
         test: /\.html$/,
         use: [
           {
             loader: "html-loader",
-            options: { minimize: true },
-          },
-        ],
+            options: { minimize: true }
+          }
+        ]
       },
       {
-        test: [/.css$|.scss$/],
+        test: [/.scss$/],
         use: [
-          // fallback to style-loader in development
-          "style-loader", MiniCssExtractPlugin.loader,
+          "style-loader",
+          MiniCssExtractPlugin.loader,
           "css-loader",
-          "sass-loader",
-        ],
-      },
-    ],
-  },
-};
+          "postcss-loader",
+          "sass-loader"
+        ]
+      }
+    ]
+  }
+}
